356cc592f35075cc672cdc0b077cb75239a746ab
SULYMAN ABDULLAHI TOPE <topeabdullahi56@outlook.com>
2023-09-13 15:12:29 -0700

    put character

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+
+/**
+ * main - the program print _putchar and followed by new line
+ *
+ * Return:return null value
+ *
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
