5d2891914f8779882ef3e88e839300b4513bc5b9
SULYMAN ABDULLAHI TOPE <topeabdullahi56@outlook.com>
2023-09-14 07:41:26 -0700

    addition of two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - the main function prints addition  two numbers
+ *@i: print int i
+ *@k: print int k
+ * Return: Always 0.
+ */
+
+int add(int i, int k)
+{
+return (i + k);
+}
